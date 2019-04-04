528fa7452a7f5350f6f3003b6f69be02f4fda897
Jordan <jordanroberts@talk21.com>
2019-04-04 08:53:58 +0100

    Completed step 11 of the Boris Bikes project by adding instance variable so that the DockingStation remembers whatever it docks. Refactored code using attr_reader

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,6 +1,15 @@
 require_relative 'bike'
 
 class DockingStation
+  attr_reader :bike
+  #This is a shorthand way of writing:
+    #def bike
+      #@bike
+    #end
+  # This essentially writes the method above on any instances of this class.
+  # The attribute reader allows us to READ the @bike attribute we set.
+  # Using attr_reader, we can now remove the #bike method we defined.
+
 
   def release_bike
     Bike.new
@@ -10,9 +19,16 @@ class DockingStation
   end
 
   def dock(bike)
+    # We need to return the bike we dock
+    # Use an instance variable to store the bike in the 'state' of this instance
+    # we need our DockingStation to REMEMBER whatever it docks and return the contents of its memory!
+    @bike = bike
   end
 
-  def bike
-  end 
+  # Let's add to our bike method to return the bike we docked:
+  #def bike
+  #  @bike
+  #end
+  #(removed because of attr_reader)
 
 end
--- spec/DockingStation_spec.rb
+++ spec/DockingStation_spec.rb
@@ -5,8 +5,15 @@ describe DockingStation do
   it { is_expected.to respond_to(:dock).with(1).argument }
   it { is_expected.to respond_to(:bike) }
 
+  #update our test for #dock
   it 'docks something' do
     bike = Bike.new
-    expect(subject.dock(bike)).to eq bike
+    expect(subject.dock(bike)).to eq bike #we want to return the bike we docked
+  end
+  #update our test for #bike
+  it 'returns docked bikes' do
+    bike = Bike.new
+    subject.dock(bike)
+    expect(subject.bike).to eq bike #again we nee to return the bike we just docked
   end
 end
